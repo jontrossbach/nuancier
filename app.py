3799462180f22010149d4011d7e67f0637f6037f
jontrossbach <jtrossba@redhat.com>
2020-07-08 16:57:30 -0400

    more changes to 8080

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from os import system
 
 if __name__ == "__main__":
     system('python3 createdb.py')
-    system('oidc-register https://iddev.fedorainfracloud.org/ http://localhost:5005/oidc_callback')
+    system('oidc-register https://iddev.fedorainfracloud.org/ http://localhost:8080/oidc_callback')
     system('python3 runserver.py -c config --port 8080')
 
