df32e376586c9084021d95663c4f0fa26e6ca8d6
jontrossbach <jtrossba@redhat.com>
2020-07-08 16:36:58 -0400

    try port 8080

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from os import system
 if __name__ == "__main__":
     system('python3 createdb.py')
     system('oidc-register https://iddev.fedorainfracloud.org/ http://localhost:5005/oidc_callback')
-    system('python3 runserver.py -c config')
+    system('python3 runserver.py -c config --port 8080')
 
